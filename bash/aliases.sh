57292069c72217f77ab2c2a883a061415a9744ee
Mateusz Łyczek <mlyczek@server.fake>
2013-10-16 10:26:24 +0200

    Added git branch name to prompt + movead all aliases definitions to aliases.sh

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,5 +1,11 @@
+#
+# Configuration of aliases
+#
+
+# enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
     alias dir='dir --color=auto'
     alias vdir='vdir --color=auto'
 
@@ -8,3 +14,7 @@ if [ -x /usr/bin/dircolors ]; then
     alias egrep='egrep --color=auto'
 fi
 
+# some more ls aliases
+#alias ll='ls -l'
+#alias la='ls -A'
+#alias l='ls -CF'
